9f9b996b74ffc1ba16d0939880d6358c343cccc5
Vladimir Krylov <leet-q3@mail.ru>
2022-09-25 12:05:42 +0200

    feat: added spring boot

---
+++ src/main/java/com/vovadev/markissuetracker/MarkIssueTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.vovadev.markissuetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkIssueTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarkIssueTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vovadev/markissuetracker/MarkIssueTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vovadev.markissuetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarkIssueTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
